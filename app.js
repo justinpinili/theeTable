8fe13f8b5a36147ee2c7d112964bd5d97a14e0b8
Justin Pinili <pinili.justin@gmail.com>
2014-11-04 10:54:43 -0800

    changed directory structure.

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-var app = require('./server/server.js');
+var express = require('express');
+var app = express();
+var db = require('./server/database.js');
+
+app.engine('html', require('ejs').renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/client');
+
+app.use(express.static(__dirname));
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
 
 app.listen(1337);
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/client');
 
-app.use(express.static(__dirname + './../client'));
+app.use(express.static(__dirname + '../'));
 
 app.get('/', function(req, res) {
   res.render('index');
