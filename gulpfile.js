9b1723c21fefb3667aec1fafbed7b2685bce3387
Justin Pinili <pinili.justin@gmail.com>
2014-11-17 22:38:51 -0800

    (fix) added jshint and nodemon to gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var nodemon = require('gulp-nodemon');
+
+gulp.task('lint', function() {
+  return gulp.src(['./*.js',
+    './server/*.js',
+    './client/**/*.js'
+    ])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
 
 gulp.task('default', function() {
   // place code for your default task here
 });
+
+gulp.task('develop', ['lint'], function () {
+  nodemon({ script: 'app.js', ext: 'html js', /*ignore: ['ignored.js']*/ })
+    .on('change', ['lint'])
+    .on('restart', function () {
+      console.log('restarted!')
+    })
+})
