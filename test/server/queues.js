de0452c40ed5933f8d3f665eb0723db7ecdac03a
Justin Pinili <pinili.justin@gmail.com>
2014-11-29 19:14:14 -0800

    (feat) all integration tests in place.

--- test/server/queues.js
+++ test/server/queues.js
@@ -1,41 +1,62 @@
-// var should          = require('should');
-// var request         = require('supertest');
-// var theeTableServer = require('./../testServer.js');
-//
-// describe('/queue API Endpoint', function() {
-//
-// 	describe('POST Request', function() {
-//
-// 		var body;
-//
-// 		it('should create a new entry in the corresponding room queue', function(done) {
-// 			request(theeTableServer)
-// 					.post('/queue')
-// 					.send({ source: 'http://youtu.be/oEm3lY3trRU' })
-// 					.expect(function(res) {
-// 						body = res.body;
-// 					})
-// 					.end(function(err, res) {
-// 						body.length.should.equal(1);
-// 						done();
-// 					});
-// 		});
-//
-// 		it('should return the full queue details', function(done) {
-// 			request(theeTableServer)
-// 					.post('/queue')
-// 					.send({ source: 'http://youtu.be/0M4nKru2H_Q' })
-// 					.expect(function(res) {
-// 						body = res.body;
-// 					})
-// 					.end(function(err, res) {
-// 						body.length.should.equal(2);
-// 						body[0].votes.should.equal(0);
-// 						body[1].votes.should.equal(0);
-// 						done();
-// 					});
-// 		});
-//
-// 	});
-//
-// });
+var should          = require('should');
+var request         = require('supertest');
+
+var express         = require('express');
+var bodyParser      = require('body-parser');
+var routes          = require('./../../server/routes.js');
+var schema          = require('./../../server/schema.js');
+var theeTableServer = express();
+
+theeTableServer.use(bodyParser.json());
+theeTableServer.use('/', routes);
+
+describe('/queue API Endpoint', function() {
+
+	before(function(done) {
+		schema.Room.where({ name: "lobby" }).findOne(function (err, room) {
+			if (!err) {
+				room.queue = [];
+				room.save();
+				done();
+				return;
+			}
+			console.log(err);
+			return;
+		});
+	});
+
+	describe('POST Request', function() {
+
+		var body;
+
+		it('should create a new entry in the corresponding room queue', function(done) {
+			request(theeTableServer)
+					.post('/queue/lobby')
+					.send({ source: 'http://youtu.be/oEm3lY3trRU' })
+					.expect(function(res) {
+						body = res.body;
+					})
+					.end(function(err, res) {
+						body.length.should.equal(1);
+						done();
+					});
+		});
+
+		it('should return the full queue details', function(done) {
+			request(theeTableServer)
+					.post('/queue/lobby')
+					.send({ source: 'http://youtu.be/0M4nKru2H_Q' })
+					.expect(function(res) {
+						body = res.body;
+					})
+					.end(function(err, res) {
+						body.length.should.equal(2);
+						body[0].votes.should.equal(0);
+						body[1].votes.should.equal(0);
+						done();
+					});
+		});
+
+	});
+
+});
