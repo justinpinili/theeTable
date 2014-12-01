ae982148c65e7f062a363ea6434501644a4ffd79
Justin Pinili <pinili.justin@gmail.com>
2014-12-01 09:47:22 -0800

    (feat) sign up now in place

--- client/js/signUpController.js
+++ client/js/signUpController.js
@@ -1,4 +1,9 @@
 angular.module('theeTable.controllers')
-	.controller('signUpController', function($scope, $state) {
-
+	.controller('signUpController', function($scope, $http) {
+		$scope.signup = function(inputUsername, inputPassword) {
+			$http.post('http://localhost:1337/user/new', {username: inputUsername, password: inputPassword})
+				.success(function(result) {
+					console.log(result);
+				});
+		}
 	});
