87c7ed2ba547416845da1c2ec05f9ba6dd9f49bf
Justin Pinili <pinili.justin@gmail.com>
2014-12-01 19:52:16 -0800

    (feat) sign in form validation in place

--- client/js/authController.js
+++ client/js/authController.js
@@ -48,15 +48,15 @@ angular.module('theeTable.controllers')
 
 		$scope.signup = function(inputUsername, inputPassword) {
 			$http.post('http://localhost:1337/user/new', {username: inputUsername, password: inputPassword})
-				// .success(function(result) {
-				// 	if (!result.message) {
-				// 		console.log(result);
-				// 		// transfer to rooms lobby
-				// 		return;
-				// 	}
-				// 	// update form validations
-				// 	console.log(result.message);
-				// 	return;
-				// });
+				.success(function(result) {
+					if (!result.message) {
+						console.log(result);
+						// transfer to rooms lobby
+						return;
+					}
+					// update form validations
+					console.log(result.message);
+					return;
+				});
 		};
 	});
--- client/js/mainController.js
+++ client/js/mainController.js
@@ -1,5 +1,22 @@
 angular.module('theeTable.controllers')
   .controller('mainController', function($scope, $state) {
 
+    $scope.current = 'login';
+
+    $scope.isCurrent = function (view) {
+      if (view === $scope.current) {
+        return true;
+      }
+      return false;
+    }
+
+    $scope.switchForm = function() {
+      if ($scope.current === 'login') {
+        $scope.current = 'signup';
+      } else {
+        $scope.current = 'login';
+      }
+    }
+
     $state.transitionTo('main.subviews');
   });
