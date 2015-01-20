255bf5e1bdc9e7eeda352d7149cbd90c68a6cba8
Justin Pinili <pinili.justin@gmail.com>
2015-01-20 15:46:52 -0800

    (feat) searching soundcloud functionality now in place

--- client/js/controllers/mainController.js
+++ client/js/controllers/mainController.js
@@ -12,4 +12,23 @@ angular.module('theeTable.controllers')
         return;
       });
     }
+
+    $scope.soundcloud = {};
+
+    $scope.search = function(query) {
+
+      SC.initialize({
+        client_id: '3fad6addc9d20754f8457461d02465f2'
+      });
+
+      SC.get('/tracks', { q: query }, function(tracks) {
+        console.log(tracks);
+        $scope.$apply(function() {
+          $scope.soundcloud.results = tracks;
+        });
+      });
+
+      $scope.soundcloud.query = '';
+
+    };
   }]);
