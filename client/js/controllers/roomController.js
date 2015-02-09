780aa3069e12848ae6e6c0c4d7ae013455e9aec9
Justin Pinili <pinili.justin@gmail.com>
2015-02-08 17:25:02 -0800

    (fix) time converter now it's own service

--- client/js/controllers/roomController.js
+++ client/js/controllers/roomController.js
@@ -1,5 +1,5 @@
 angular.module('theeTable.controllers')
-	.controller('roomController', ['$scope', '$state', '$stateParams', '$location', '$sce', 'localStorageService', 'theeTableAuth', 'theeTableRooms', function($scope, $state, $stateParams, $location, $sce, localStorageService, theeTableAuth, theeTableRooms) {
+	.controller('roomController', ['$scope', '$state', '$stateParams', '$location', '$sce', 'localStorageService', 'theeTableAuth', 'theeTableRooms', 'theeTableTime', function($scope, $state, $stateParams, $location, $sce, localStorageService, theeTableAuth, theeTableRooms, theeTableTime) {
 
 		/*************
 		 * Socket.IO *
@@ -187,23 +187,7 @@ angular.module('theeTable.controllers')
 		};
 
 		$scope.convertTime = function(duration) {
-			var hours = Math.floor(duration / 3600000);
-			var minutes = Math.floor((duration % 3600000) / 60000);
-			var seconds = Math.floor(((duration % 360000) % 60000) / 1000);
-
-			if (seconds < 10) {
-				seconds = "0"+seconds;
-			}
-
-			if (minutes < 10) {
-				minutes = "0"+minutes;
-			}
-
-			if (hours > 0) {
-				return hours + ":" + minutes + ":" + seconds;
-			}
-
-			return minutes + ":" + seconds;
+			return theeTableTime.convertTime(duration);
 		};
 
 	}]);
