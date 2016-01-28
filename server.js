fa8ebc576762a73cae5d48f205818328d5d160a0
Justin Pinili <pinili.justin@gmail.com>
2016-01-27 20:29:18 -0800

    hiding dist folder

--- server.js
+++ server.js
@@ -10,16 +10,14 @@ var routes = require('./server/routes.js');
 var socketIO = require('./server/routes/socketIO.js')(io);
 
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/dist');
+app.use(express.static(__dirname + '/dist'));
 
 if (process.env.PORT) {
   var ttURL = 'http://www.theetable.io';
-  app.set('views', __dirname + '/dist');
-  app.use(express.static(__dirname + '/dist'));
   var keys = require('./server/exampleSecurityKeys.js');
 } else {
   var ttURL = 'http://localhost:1337';
-  app.set('views', __dirname + '/client');
-  app.use(express.static(__dirname + '/client'));
   var keys = require('./server/securityKeys.js');
 }
 
